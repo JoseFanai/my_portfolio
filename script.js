8e3a2441f79e36c0f25c833613eeaf35dc19e916
Your Name <josefanai0102@gmail.com>
2026-05-25 19:19:40 +0530

    contact section update

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ const submitMail = document.getElementById('mail-submit')
 submitMail.onclick = () => {
     
 }
+
+const textarea = document.getElementById('autoExpand');
+
+        textarea.addEventListener('input', function() {
+            this.style.height = 'auto'; /* Height a reset phawt ang */
+            this.style.height = (this.scrollHeight) + 'px'; /* A thu ziah len dan (scrollHeight) azirin a ti lian ang */
+        });
